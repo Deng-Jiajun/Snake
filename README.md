996ec1f4ad2d5b14c1bcfc3dc30e632b852ee870
邓佳俊 <deng-jiajun@outlook.com>
2022-01-30 19:53:56 +0800

    项目搭建
    项目搭建及插件配置测试已完成

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # Snake
- TypeScript 小项目
+
+> TypeScript 小项目 **贪吃蛇**（from https://www.bilibili.com/video/BV1Xy4y1v7S2 ）
+
+## 项目搭建
+
+1. 复制 `./packagesource` 中的三个配置文件
+2. 下载相关依赖 `npm i`
+3. 创建 `./src/index.html` 和 `./src/index.ts`
+4. `npm rum build`，查看是否编译成功
+
+### 配置 CSS
+
+1. 安装 less 相关插件：`npm i -D less less-loader css-loader style-loader`
+2. 添加对 less 文件的 rule
+   ```JavaScript
+      // 对 .less 文件的处理
+      {
+        test: /\.less$/,
+        // less → css → style
+        use: ["style-loader", "css-loader", "less-loader"],
+      }
+   ```
+3. 新建 `./src/style/index.less`
+   ```less
+   body {
+     background-color: #bfa;
+   }
+   ```
+4. 在 `index.ts` 中引入
+   ```typescript
+   import "./style/index.less";
+   ```
+5. `npm rum build`，查看是否编译成功（背景变绿，编译成功）
+
+### 旧版本 CSS 兼容
+
+1.  安装插件 `npm i -D postcss postcss-loader postcss-preset-env`
+2.  在针对 `.less` 文件的 rules 的 `css-loader` 和 `less-loader` 间插入 `post-loader`
+    ```javascript
+          // 对 .less 文件的处理
+      {
+        test: /\.less$/,
+        // less → css → style
+        use: [
+          "style-loader",
+          "css-loader",
+          // 引入 postcss
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      browsers: "last 2 versions",
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          "less-loader",
+        ],
+      },
+    ```
+3.  index.less 中添加 `flex`
+    ```less
+    body {
+      background-color: #bfa;
+      display: flex;
+    }
+    ```
+4.  `npm rum build`，查看是否编译成功
---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+// 引入一个用于拼接路径的包
+const path = require("path");
+
+// 引入 html 插件
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+// 引入 clean 插件
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+// webpack 中的所有配置信息都应该写在 module.exports 中
+module.exports = {
+  // 生产模式
+  mode: "production",
+  // 指定入口文件
+  entry: "./src/index.ts",
+
+  // 指定打包文件所在目录
+  output: {
+    // 清除 output（每次输出前）
+    clean: true,
+    // 指定打包文件的目录
+    path: path.resolve(__dirname, "dist"),
+
+    // 打包后的文件名
+    filename: "bundle.js",
+
+    // 要求 webpack 不使用箭头函数
+    environment: {
+      arrowFunction: false,
+    },
+  },
+
+  // 指定 webpack 打包时要使用的模块
+  module: {
+    // 指定要加载的规则
+    rules: [
+      // 对 .ts 文件的处理
+      {
+        // test 指定的是规则生效的文件
+        test: /\.ts$/,
+        // 要使用的 loader(loader 从后往前依次执行)
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  // bable 的预配置信息
+                  "@babel/preset-env",
+                  {
+                    // 要兼容的目标浏览器
+                    targets: {
+                      chrome: "88",
+                      ie: "11",
+                    },
+                    // 指定 core-js 的版本
+                    corejs: "3",
+                    // 使用 core-js 的方式：usage 表示按需加载
+                    useBuiltIns: "usage",
+                  },
+                ],
+              ],
+            },
+          },
+          "ts-loader",
+        ],
+        // 要排除的文件
+        exclude: /node-modules/,
+      },
+
+      // 对 .less 文件的处理
+      {
+        test: /\.less$/,
+        // less → css → style
+        use: [
+          "style-loader",
+          "css-loader",
+          // 引入 postcss
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      browsers: "last 2 versions",
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          "less-loader",
+        ],
+      },
+    ],
+  },
+
+  // 配置 webpack 插件
+  // plugins: [new HTMLWebpackPlugin({title: "Snake"})],
+  plugins: [new HTMLWebpackPlugin({ template: "./src/index.html" })],
+
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
