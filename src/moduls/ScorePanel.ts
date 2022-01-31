4dcd19cad109cf04c14e8550db64618e15bf7ff8
邓佳俊 <deng-jiajun@outlook.com>
2022-01-31 20:24:18 +0800

    ScorePanel 类
    完成 ScorePanel 类，并划分模块，将一个类拆分到一个文件内

---
+++ src/moduls/ScorePanel.ts
@@ -0,0 +1,40 @@
+// 定义 ScorePanel 类
+class ScorePanel {
+    score = 0;
+    level = 1;
+
+    // 最高等级
+    maxLevel: number;
+
+    // 升级门槛
+    upScore: number;
+
+
+    scoreElement: HTMLElement;
+    levelElement: HTMLElement;
+
+    // 构造函数，参数后跟默认值
+    constructor(maxLevel: number = 10,upScore:number =10) {
+        this.scoreElement = document.getElementById("score")!;
+        this.levelElement = document.getElementById("level")!;
+        this.maxLevel = maxLevel;
+        this.upScore = upScore;
+    }
+
+    // 设置一个加分方法
+    addScore() {
+        this.score++;
+        this.scoreElement.innerHTML = this.score + '';
+
+        if (this.score % this.upScore === 0)
+            this.levelUp();
+    }
+
+    // 设置一个升级方法
+    levelUp() {
+        if (this.level < this.maxLevel)
+            this.levelElement.innerHTML = ++this.level + '';
+    }
+}
+
+export default ScorePanel;
